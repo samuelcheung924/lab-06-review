9f4f44106d6440773c438ad3616ebfd23e742ea3
samuelcheung924 <67924107+samuelcheung924@users.noreply.github.com>
2020-10-14 20:05:44 -0400

    update as of 20201014@8PM

---
+++ BMI.py
@@ -0,0 +1,8 @@
+import sys
+
+print ("BMI: BODY MASS INDEX V0.1 gamma beta whatevs")
+userWeight = input("Enter Your Weight (in Pounds):")
+userHeight = input("Enter Your Weight (in inches):")
+
+bmi = (703 * float(userWeight)) / (float(userHeight) * float(userHeight))
+print ("Your body mass index(BMI) is:" + str(BMI) + "%")
